322481a36a08ca75b8d3b72be0c8a65a98abf49e
RalphBrynard <ralph@thebrynards.com>
2021-12-07 09:57:04 -0600

    removed variables.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,35 @@ provider "aws" {
   region = "us-east-2"
 }
 
-variable "device_name" {}
-variable "aws_account" {}
+variable "vpc_name" {
+  description = "Name of the VPC to deploy in the AWS account."
+}
+variable "aws_region" {
+  description = "Region in which to deploy the VPC."
+  default     = "us-east-2"
+}
+variable "aws_account" {
+  description = "The AWS Account ID."
+}
+variable "aws_account_alias" {
+  description = "AWS Account alias."
+  default     = ""
+}
+variable "customer_gateway_ip" {
+  description = "IP address of the customer gateway."
+}
+variable "device_name" {
+  description = "Customer gateway device name."
+}
+variable "environment" {
+  description = "Deployment environment. Possible values: Prod, Staging, Test, Dev."
+  default     = "dev"
+}
+variable "cidr" {
+  description = "CIDR range for VPC deployment. Possible values: 10.0.0.0/16, 20.10.0.0/16"
+}
+variable "build_branch" {}
+variable "build_repo" {}
 
 locals {
   name         = var.vpc_name
